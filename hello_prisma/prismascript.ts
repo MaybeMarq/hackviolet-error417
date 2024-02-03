e9dfa05ddd3a700d334a1d7ea711b9a3797b4fbb
shreyaorange28 <118227225+shreyaorange28@users.noreply.github.com>
2024-02-03 15:04:21 -0500

    functional database

--- hello_prisma/prismascript.ts
+++ hello_prisma/prismascript.ts
@@ -3,41 +3,45 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-    // adds user to data base 
-    // const user = await prisma.user.create({
-    //     data: {
-    //       name: 'Alice',
-    //       email: 'alice@prisma.io',
-    //     },
-    //   })
-    //   console.log(user)
+  // adds user to data base 
+  // const apartment = await prisma.apartment.create({
+  //   data: {
+  //     name: 'Terrace View',
+  //     location: '413 Hunt Club Rd, Blacksburg, VA 24060',
+  //   },
+  // })
+  // console.log(apartment)
 
-    //gets all of the current users
-    const users = await prisma.user.findMany() 
-    console.log(users)
+  //gets all of the current users
+  // const apartments = await prisma.apartment.findMany() 
+  // console.log(apartments)
 
 
-    //creates both user and post at once
-    // const user = await prisma.user.create({
-    //     data: {
-    //       name: 'Bob',
-    //       email: 'bob@prisma.io',
-    //       posts: {
-    //         create: {
-    //           title: 'Hello World',
-    //         },
-    //       },
-    //     },
-    //   })
-    //   console.log(user)
+  //creates both apartment and post at once
+  // const apartment = await prisma.apartment.create({
+  //   data: {
+  //     name: 'Vue',
+  //     location: '1902 Research Center Dr, Blacksburg, VA',
+  //     posts: {
+  //       create: {
+  //         title: 'Good Apartment',
+  //         user: "lovingthevue",
+  //         overall: 4.5,
+  //         content: "Liked living there",
+  //         published: true
+  //       },
+  //     },
+  //   },
+  // })
+  // console.log(apartment)
 
-    //gets users and posts 
-    // const usersWithPosts = await prisma.user.findMany({
-    //     include: {
-    //       posts: true,
-    //     },
-    //   })
-    //   console.dir(usersWithPosts, { depth: null })
+  //gets users and posts 
+  const usersWithPosts = await prisma.apartment.findMany({
+    include: {
+      posts: true,
+    },
+  })
+  console.dir(usersWithPosts, { depth: null })
 }
 
 main()
